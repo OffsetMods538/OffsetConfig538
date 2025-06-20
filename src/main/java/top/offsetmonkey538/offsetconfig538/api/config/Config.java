9096c8c97535cf1113275561d247406ecadf7ee9
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2025-06-20 23:04:18 +0300

    Mark 'getName' and 'getConfigDir' as deprecated so I don't accidentally use themmmmm

--- src/main/java/top/offsetmonkey538/offsetconfig538/api/config/Config.java
+++ src/main/java/top/offsetmonkey538/offsetconfig538/api/config/Config.java
@@ -3,22 +3,24 @@ package top.offsetmonkey538.offsetconfig538.api.config;
 import blue.endless.jankson.Jankson;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Range;
 
 import java.nio.file.Path;
 
 public interface Config {
 
 
-    @Contract(value = "_ -> param1", pure = false)
+    @Contract
     default void configureJankson(final @NotNull Jankson.Builder builder) {
         // no-op
     }
 
+    @Contract(pure = true)
     default @NotNull Datafixer[] getDatafixers() {
         return new Datafixer[] {};
     }
 
-    default int getConfigVersion() {
+    default @Range(from = 0, to = Integer.MAX_VALUE) int getConfigVersion() {
         return 0;
     }
 
@@ -26,6 +28,38 @@ public interface Config {
         return getConfigDir().resolve(getName() + ".json");
     }
 
+
+    /**
+     * Used for building the config file path.
+     * <p>
+     *     <strong>Deprecated because this method should not be called! Please call {@link Path#getFileName()} on the {@link Path} returned from {@link #getFilePath()} instead!</strong>
+     * </p>
+     * <p>
+     *     <strong>It is fine (recommended even) to implement this method! Only deprecated because it's fine to just override {@link #getFilePath()} and not use this method.</strong>
+     * </p>
+     *
+     * @deprecated because this shouldn't be called. It is fine to implement.
+     * @return The name of the config file.
+     * @see #getFilePath()
+     * @see #getConfigDir()
+     */
+    @Deprecated
     @NotNull String getName();
+
+    /**
+     * Used for building the config file path.
+     * <p>
+     *     <strong>Deprecated because this method should not be called! Please call {@link Path#getParent()} on the {@link Path} returned from {@link #getFilePath()} instead!</strong>
+     * </p>
+     * <p>
+     *     <strong>It is fine (recommended even) to implement this method! Only deprecated because it's fine to just override {@link #getFilePath()} and not use this method.</strong>
+     * </p>
+     *
+     * @deprecated because this shouldn't be called. It is fine to implement.
+     * @return The directory the config will be in.
+     * @see #getFilePath()
+     * @see #getName()
+     */
+    @Deprecated
     @NotNull Path getConfigDir();
 }
